54d0fae21069fcebc584911598d1010b41663453
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-05 00:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function toCase(text) {
   // write your code here
-	if (str=='') return "-"
+	if (str==='') return "-"
 	let lower=text.toLowerCase()
 	let upper=text.toUpperCase()
+	
 	return lower+"-"+upper
 }
 
 // DO not change the code below
 
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(toCase(text));
